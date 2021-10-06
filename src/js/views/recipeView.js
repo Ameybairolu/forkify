4f8fff912ee2ff8ab497f4e2329134fc05510160
Ameybairolu <ameybairolu@gmail.com>
2021-10-06 19:21:23 +0530

    fractional changes

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,7 @@
 import View from './view.js';
 
 import icons from 'url:../../img/icons.svg';
+const Fraction = require('fractional').Fraction;
 class RecipeView extends View {
 
   _errorMessage = `We couldn't find that recipe. Please try another one!`;
@@ -120,7 +121,7 @@ class RecipeView extends View {
                           <svg class="recipe__icon">
                             <use href="${icons}#icon-check"></use>
                           </svg>
-                          <div class="recipe__quantity">${ing.quantity ? ing.quantity : ''
+                          <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''
       }</div >
         <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
